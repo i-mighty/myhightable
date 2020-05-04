18c1bf823084fcae2b76b1e3b1fb930add41de88
i-mighty <josadegboye@gmail.com>
2020-05-04 02:33:34 +0100

    feat: Added TS, Git hooks, Prettier and Linting

--- HighTable/.eslintrc.js
+++ HighTable/.eslintrc.js
@@ -1,4 +1,22 @@
 module.exports = {
   root: true,
-  extends: '@react-native-community',
+  extends: ['airbnb', 'airbnb/hooks', '@react-native-community'],
+  plugins: ['prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+    'no-unused-vars': 'warn',
+    'no-console': 1,
+    'object-curly-newline': 0,
+    'react/jsx-filename-extension': 0,
+  },
+  parserOptions: {
+    sourceType: 'module',
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    },
+  },
 };
