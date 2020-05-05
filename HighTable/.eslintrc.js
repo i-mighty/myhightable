0df08c783207c5c128161a80dad6279a0e16f4c4
i-mighty <josadegboye@gmail.com>
2020-05-05 01:06:10 +0100

    Setup unit tests

--- HighTable/.eslintrc.js
+++ HighTable/.eslintrc.js
@@ -11,6 +11,12 @@ module.exports = {
     'object-curly-newline': 0,
     'react/jsx-filename-extension': 0,
     'eslint-comments/no-unlimited-disable': 0,
+    'import/no-extraneous-dependencies': [
+      'error',
+      {
+        devDependencies: ['**/*.test.ts', '**/*.test.tsx'],
+      },
+    ],
   },
   parserOptions: {
     sourceType: 'module',
--- HighTable/jest.config.js
+++ HighTable/jest.config.js
@@ -1,4 +1,6 @@
 module.exports = {
-  preset: 'react-native',
+  preset: '@testing-library/react-native',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  setupFilesAfterEnv: ['@testing-library/react-native/cleanup-after-each'],
+  testPathIgnorePatterns: ['/node_modules', '/e2e/'],
 };
